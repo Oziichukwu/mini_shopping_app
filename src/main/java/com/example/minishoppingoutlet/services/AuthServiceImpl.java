9d2de715f5047e9eb280aca6bdf9168180bfc518
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-01 00:23:15 +0100

    auth service implemntation

--- src/main/java/com/example/minishoppingoutlet/services/AuthServiceImpl.java
+++ src/main/java/com/example/minishoppingoutlet/services/AuthServiceImpl.java
@@ -1,15 +1,26 @@
 package com.example.minishoppingoutlet.services;
 
 import com.example.minishoppingoutlet.data.dtos.request.CreateUserRequest;
+import com.example.minishoppingoutlet.data.dtos.request.LoginRequest;
 import com.example.minishoppingoutlet.data.dtos.response.CreateUserResponse;
+import com.example.minishoppingoutlet.data.dtos.response.JwtResponseToken;
 import com.example.minishoppingoutlet.data.models.User;
 import com.example.minishoppingoutlet.data.repositories.UserRepository;
 import com.example.minishoppingoutlet.exceptions.EmailNotFoundException;
+import com.example.minishoppingoutlet.security.CustomUserDetailService;
+import com.example.minishoppingoutlet.security.JwtTokenProvider;
+import com.example.minishoppingoutlet.security.UserPrincipal;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.security.auth.message.AuthException;
 
 @Service
 @Slf4j
@@ -20,6 +31,15 @@ public class AuthServiceImpl implements AuthService {
 
     @Autowired
     ModelMapper modelMapper;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
@@ -37,6 +57,27 @@ public class AuthServiceImpl implements AuthService {
 
         return modelMapper.map(savedUser, CreateUserResponse.class);
     }
+
+    @Override
+    public JwtResponseToken login(LoginRequest loginRequest){
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(), loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final UserPrincipal userDetails = (UserPrincipal) customUserDetailService.loadUserByUsername(loginRequest.getEmail());
+        final String token = jwtTokenProvider.generateToken(userDetails);
+        User user = internalFindUserByEmail(loginRequest.getEmail());
+        return new JwtResponseToken(token , user.getEmail());
+    }
+
+    private User internalFindUserByEmail(String email) {
+        return userRepository.findUserByEmail(email).orElse(null);
+    }
+
 
     private User save(User user) {
         return userRepository.save(user);
