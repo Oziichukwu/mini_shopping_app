fabd5b4e80e4c03ca08bb9c021081b225aea5762
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-01 00:24:26 +0100

    security

--- src/main/java/com/example/minishoppingoutlet/security/CustomUserDetailService.java
+++ src/main/java/com/example/minishoppingoutlet/security/CustomUserDetailService.java
@@ -1,2 +1,28 @@
-package com.example.minishoppingoutlet.security;public class CustomUserDetailService {
+package com.example.minishoppingoutlet.security;
+
+import com.example.minishoppingoutlet.data.models.User;
+import com.example.minishoppingoutlet.data.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+
+import static java.lang.String.format;
+
+
+@Component
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findUserByEmail(email).orElseThrow(()->
+                new UsernameNotFoundException(format("user with email does not exist %s", email)));
+        return UserPrincipal.create(user);
+    }
 }
--- src/main/java/com/example/minishoppingoutlet/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/minishoppingoutlet/security/JwtAuthenticationFilter.java
@@ -1,2 +1,69 @@
-package com.example.minishoppingoutlet.security;public class JwtAuthenticationFilter {
+package com.example.minishoppingoutlet.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String email = null;
+        String jwtToken = getTokenFromRequest(request);
+
+        if (jwtToken != null) {
+            email = jwtTokenProvider.extractEmail(jwtToken);
+        }
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+            UserDetails userDetails = customUserDetailService.loadUserByUsername(email);
+
+            if (jwtTokenProvider.validateToken(jwtToken, userDetails)) {
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+           jwt = authorizationHeader.substring(7);
+        }
+
+        return jwt;
+    }
 }
