eb0c9905aae08575832f15b6aeecf81b4783d009
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-01 10:25:46 +0100

    token exception implemented

--- src/main/java/com/example/minishoppingoutlet/exceptions/AuthenticationException.java
+++ src/main/java/com/example/minishoppingoutlet/exceptions/AuthenticationException.java
@@ -1,2 +1,19 @@
-package com.example.minishoppingoutlet.exceptions;public class AuthenticationException {
+package com.example.minishoppingoutlet.exceptions;
+
+public class AuthenticationException extends MiniShoppingException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthenticationException(Throwable cause) {
+        super(cause);
+    }
+
+    public AuthenticationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/main/java/com/example/minishoppingoutlet/exceptions/TokenException.java
+++ src/main/java/com/example/minishoppingoutlet/exceptions/TokenException.java
@@ -1,2 +1,19 @@
-package com.example.minishoppingoutlet.exceptions;public class TokenException {
+package com.example.minishoppingoutlet.exceptions;
+
+public class TokenException extends MiniShoppingException {
+    public TokenException(String message) {
+        super(message);
+    }
+
+    public TokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TokenException(Throwable cause) {
+        super(cause);
+    }
+
+    public TokenException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
