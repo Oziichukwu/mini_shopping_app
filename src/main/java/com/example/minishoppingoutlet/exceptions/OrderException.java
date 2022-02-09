537f466924fcfd8c72509788cc8f2daf3b50fa3a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-09 05:07:06 +0100

    exception order

--- src/main/java/com/example/minishoppingoutlet/exceptions/OrderException.java
+++ src/main/java/com/example/minishoppingoutlet/exceptions/OrderException.java
@@ -1,2 +1,20 @@
-package com.example.minishoppingoutlet.exceptions;public class OrderException {
+package com.example.minishoppingoutlet.exceptions;
+
+public class OrderException extends MiniShoppingException {
+
+    public OrderException(String message) {
+        super(message);
+    }
+
+    public OrderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OrderException(Throwable cause) {
+        super(cause);
+    }
+
+    public OrderException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
