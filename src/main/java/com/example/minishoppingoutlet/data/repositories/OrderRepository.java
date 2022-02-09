04672b82805a51013922a70acd86dce562080288
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-09 04:03:10 +0100

    repositiory added

--- src/main/java/com/example/minishoppingoutlet/data/repositories/OrderRepository.java
+++ src/main/java/com/example/minishoppingoutlet/data/repositories/OrderRepository.java
@@ -1,2 +1,8 @@
-package com.example.minishoppingoutlet.data.repositories;public interface OrderRepository {
+package com.example.minishoppingoutlet.data.repositories;
+
+import com.example.minishoppingoutlet.data.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, String> {
+    void findByOrderId(String orderId);
 }
--- src/main/java/com/example/minishoppingoutlet/data/repositories/RoleRepository.java
+++ src/main/java/com/example/minishoppingoutlet/data/repositories/RoleRepository.java
@@ -1,2 +1,12 @@
-package com.example.minishoppingoutlet.data.repositories;public class RoleRepository {
+package com.example.minishoppingoutlet.data.repositories;
+
+import com.example.minishoppingoutlet.data.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, String> {
+
+    Optional<Role> findRoleByName(String roleName);
 }
