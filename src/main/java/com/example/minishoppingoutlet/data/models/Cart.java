5a5a450c952dc23b35c79b03376031fe8594ed5c
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-13 14:37:42 +0100

    cart and cartItem model updated

--- src/main/java/com/example/minishoppingoutlet/data/models/Cart.java
+++ src/main/java/com/example/minishoppingoutlet/data/models/Cart.java
@@ -1,2 +1,63 @@
-package com.example.minishoppingoutlet.data.models;public class Cart {
+package com.example.minishoppingoutlet.data.models;
+
+import com.example.minishoppingoutlet.exceptions.CartException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private BigDecimal total;
+
+    @Transient
+    private Map<String, Item>items = new HashMap<>();
+
+    public void addItem(Product product, int quantity){
+        String productId = product.getProductId();
+        Item item = new Item();
+        item.setProduct(product);
+
+        if (items.containsKey(productId))
+            items.get(productId).increaseQuantity(quantity);
+        else items.put(productId, item);
+        total = calculateTotal();
+    }
+
+    public void removeItem(String productId){
+        items.remove(productId);
+        total = calculateTotal();
+    }
+
+    public void removeItem(String productId, int quantity){
+        if (items.containsKey(productId)){
+            items.get(productId).decreaseQuantity(quantity);
+        }else {
+            throw new CartException("item is not int cart");
+        }
+        total = calculateTotal();
+    }
+
+    private BigDecimal calculateTotal() {
+
+        BigDecimal total = BigDecimal.ZERO;
+        for (Item cartItem : items.values()){
+            total = total.add(cartItem.getItemTotal());
+        }
+        return total;
+    }
+
 }
--- src/main/java/com/example/minishoppingoutlet/data/models/Item.java
+++ src/main/java/com/example/minishoppingoutlet/data/models/Item.java
@@ -1,2 +1,39 @@
-package com.example.minishoppingoutlet.data.models;public class Item {
+package com.example.minishoppingoutlet.data.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+
+
+@Data
+public class Item {
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private Product product;
+
+    private int quantityAddedToCart = 1;
+
+    private BigDecimal itemTotal;
+
+    private BigDecimal updateProductAmount(int quantityAddedToCart){
+        return product.getPrice().multiply(BigDecimal.valueOf(quantityAddedToCart));
+    }
+
+    public void increaseQuantity(int quantityAddedToCart){
+        this.quantityAddedToCart += quantityAddedToCart;
+        itemTotal = updateProductAmount(quantityAddedToCart);
+    }
+
+    public void decreaseQuantity(int quantityAddedToCart){
+        this.quantityAddedToCart -= quantityAddedToCart;
+        itemTotal = updateProductAmount(quantityAddedToCart);
+    }
+
 }
