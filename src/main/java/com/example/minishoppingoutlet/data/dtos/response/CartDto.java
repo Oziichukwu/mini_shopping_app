da31b5ad38143cbb430d5f436d2854c22e0cb659
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-13 14:38:24 +0100

    product and cart dtos implemented

--- src/main/java/com/example/minishoppingoutlet/data/dtos/response/CartDto.java
+++ src/main/java/com/example/minishoppingoutlet/data/dtos/response/CartDto.java
@@ -1,2 +1,24 @@
-package com.example.minishoppingoutlet.data.dtos.response;public class CartDto {
+package com.example.minishoppingoutlet.data.dtos.response;
+
+
+import com.example.minishoppingoutlet.data.models.Item;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartDto {
+
+    @NotBlank
+    private String id;
+
+    private BigDecimal total;
+
+    private Map<String, Item> items;
 }
